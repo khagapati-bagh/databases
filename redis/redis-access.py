379701ac99b58600e740df33c0a73aa80c9ebd6b
khagapati-bagh <mr.k.affinity@gmail.com>
2022-10-24 17:59:46 +0530

    Adeed multiple key value pair example

--- redis/redis-access.py
+++ redis/redis-access.py
@@ -1,6 +1,17 @@
 import redis
 
-redis = redis.Redis(host= 'localhost', port= '6379')
+redis = redis.Redis(host='localhost', port='6379')
+
+# Simple key value pair
 redis.set('mypykey', 'Hello from Python!')
 value = redis.get('mypykey')
+print(value)
+
+# Multiple-key value pairs
+data = {
+    'fest': 'hacktoberfest',
+    'year': '2022'
+}
+redis.mset(data)
+value = redis.get('fest')
 print(value)
